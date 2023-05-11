c6df2eac0ce19f405c9856f86ff1b02070ca2d57
zamanof <nadir.r.zamanov@gmail.com>
2023-05-11 18:11:55 +0400

    hooks start 2

---
+++ React lessons/hooksintro/src/components/App.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import SomeComponent from "./someComponent";
+import Users from "./users";
+
+function App() {
+    return (
+        <div>
+           {/*<SomeComponent some={"Some message"}/>*/}
+            <Users/>
+        </div>
+    );
+}
+
+export default App;
---
+++ React lessons/hooksintro/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from "./components/App";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>
+);
+
+// stateful components - class components
+
+// stateless components - function components
+
+// props drill
+

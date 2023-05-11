c99dc9453368bcdc66fa275350e2e9db44248e3a
zamanof <nadir.r.zamanov@gmail.com>
2023-05-11 19:07:39 +0400

    Eroor solution 2

--- React lessons/hooksintro/src/components/App.js
+++ React lessons/hooksintro/src/components/App.js
@@ -1,30 +1,17 @@
 import React, {useState} from 'react';
-<<<<<<< HEAD
 // import SomeComponent from "./someComponent";
 import Users from "./Users";
 import { UserProvider} from "./context";
-=======
-import SomeComponent from "./someComponent";
-import Users from "./users";
->>>>>>> c6df2eac0ce19f405c9856f86ff1b02070ca2d57
 
 function App() {
     return (
         <div>
            {/*<SomeComponent some={"Some message"}/>*/}
-<<<<<<< HEAD
            <UserProvider>
-                <Users/>
-           </UserProvider>
-=======
             <Users/>
->>>>>>> c6df2eac0ce19f405c9856f86ff1b02070ca2d57
+           </UserProvider>
         </div>
     );
 }
 
-<<<<<<< HEAD
 export default App;
-=======
-export default App;
->>>>>>> c6df2eac0ce19f405c9856f86ff1b02070ca2d57
--- React lessons/hooksintro/src/index.js
+++ React lessons/hooksintro/src/index.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
-
-=======
->>>>>>> c6df2eac0ce19f405c9856f86ff1b02070ca2d57
 import App from "./components/App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));

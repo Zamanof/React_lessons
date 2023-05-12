8a6ee4113e97b0a23c3f104c69bc3239032c1c08
zamanof <nadir.r.zamanov@gmail.com>
2023-05-12 22:10:27 +0400

    lifecycle methods react

---
+++ React lessons/lifecycle/src/App.js
@@ -0,0 +1,25 @@
+import React, {useState, useEffect } from "react";
+import LifeCycleMethods from "./components/life-cycle-methods";
+import {cleanup} from "@testing-library/react";
+function App() {
+    const [timer, setTimer] = useState(50)
+    const [salam, setSalam] = useState(50)
+    // setInterval(()=>{setTimer(timer - 1)}, 1000)
+    useEffect(()=>{
+        const interval =
+            setInterval(()=>{setTimer(timer - 1)}, 1000)
+        return()=>clearInterval(interval)
+    }, [timer])
+    // useEffect(()=>console.log("useEffect"), [timer])
+  return (
+    <div>
+        {/*<LifeCycleMethods/>*/}
+        <button onClick={()=>setSalam(salam + 1)}>Salam</button>
+        <p>Salam: {salam}</p>
+        <p>Timer: {timer}</p>
+        {/*<button onClick={()=>setTimer(timer - 1)}>Timer</button>*/}
+
+    </div>
+  );
+}
+export default App;
